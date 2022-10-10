d90e65a3627e1eaba19f80509b74fe09c4b06bc1
abdullah43577 <100965547+abdullah43577@users.noreply.github.com>
2022-10-10 15:19:15 +0100

    first deployment

---
+++ script.js
@@ -0,0 +1,67 @@
+"use strict";
+
+// 1. listen for click in the 'Add new' button
+const noteContent = [];
+const addNote = document.querySelector("#addNote");
+const noteInput = document.querySelector("#noteInput");
+const noteSection = document.querySelector("#noteSection");
+const viewMore = document.querySelectorAll(".show-Modal");
+const modalMessage = document.querySelector(".paragraph");
+const modalContainer = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+//console.log(modalContainer);
+const closeModal = document.querySelector(".close-modal");
+
+// function for closing down the modal popup window
+const closeModalDown = () => {
+  modalContainer.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+addNote.addEventListener("click", () => {
+  //console.log(`Add new button is being clicked`);
+  //2. get in the input value in the input box and store in an array
+  if (noteInput.value !== "") {
+    //console.log(noteInput.value);
+    noteContent.push(noteInput.value);
+
+    let textContent = "";
+    for (let i = 0; i < noteContent.length; i++) {
+      textContent = `<div class="container">
+                          <h3>Note ${i + 1}</h3>
+                          <p class="messageEl">${noteInput.value}</p>
+                          <button class="show-Modal" onclick="showModal(event)">View Details</button>  
+                    </div>`;
+      console.log(textContent);
+    }
+    noteSection.innerHTML += textContent;
+    textContent = "";
+  }
+  // empty the input field
+  noteInput.value = "";
+});
+
+//showModal popup function
+function showModal(e) {
+  //console.log("I'm being clicked");
+  //assign the input value of the noteInput section to the innerHTML of the div container
+  for (let i = 0; i < noteContent.length; i++) {
+    modalMessage.innerHTML = noteContent[i];
+  }
+  // modalMessage.innerHTML = noteInput.value;
+  //remove the hidden class from the div modal class
+  modalContainer.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+  //console.log(modalContainer);
+}
+
+//close Modal Button function call
+closeModal.addEventListener("click", closeModalDown);
+
+//listening for the escape key to close down the modal
+document.addEventListener("keydown", function (e) {
+  // if the press key is 'Escape' and does not contains the class of hidden
+  if (e.key === "Escape" && !modalContainer.classList.contains("hidden")) {
+    closeModalDown();
+  }
+});
